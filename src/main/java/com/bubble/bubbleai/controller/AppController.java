8759e2ca1afa29a4f725251a5153e3eee51df3ce
CodeTainian <sxtianian@outlook.com>
2026-06-29 16:16:17 +0800

    Implemented API rate limiting using Redisson and Spring AOP, thereby enhancing security.

--- src/main/java/com/bubble/bubbleai/controller/AppController.java
+++ src/main/java/com/bubble/bubbleai/controller/AppController.java
@@ -14,6 +14,8 @@ import com.bubble.bubbleai.model.dto.app.*;
 import com.bubble.bubbleai.model.entity.App;
 import com.bubble.bubbleai.model.entity.User;
 import com.bubble.bubbleai.model.vo.AppVO;
+import com.bubble.bubbleai.ratelimiter.annotation.RateLimit;
+import com.bubble.bubbleai.ratelimiter.enmus.RateLimitType;
 import com.bubble.bubbleai.service.AppService;
 import com.bubble.bubbleai.service.ProjectDownloadService;
 import com.bubble.bubbleai.service.UserService;
@@ -284,6 +286,7 @@ public class AppController {
      * @return 生成结果流
      */
     @GetMapping(value = "/chat/gen/code",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @RateLimit(limitType = RateLimitType.USER, rate = 5, rateInterval = 60, message = "AI 对话请求过于频繁，请稍后再试")
     public Flux<ServerSentEvent<String>> chatToGenCode(@RequestParam Long appId, @RequestParam String message, HttpServletRequest request) {
         //参数校验
         ThrowUtils.throwIf(appId==null||appId <= 0, ErrorCode.PARAMS_ERROR,"应用ID无效");
