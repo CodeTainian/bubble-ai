26fc0facd1b73b4ebc43d2139534290eea26ec14
CodeTainian <sxtianian@outlook.com>
2026-07-03 23:04:07 +0800

    common-microservice

---
+++ bubble-ai-microservice/bubble-ai-code-common/src/main/java/com/bubble/bubbleai/common/PageRequest.java
@@ -0,0 +1,23 @@
+package com.bubble.bubbleai.common;
+
+import lombok.Data;
+
+@Data
+public class PageRequest {
+    /**
+     * 当前页号
+     */
+    private int pageNum = 1;
+    /**
+     * 页面大小
+     */
+    private int pageSize = 5;
+    /**
+     * 排序字段
+     */
+    private String sort;
+    /**
+     * 排序顺序(默认降序)
+     */
+    private String order="descend";
+}
